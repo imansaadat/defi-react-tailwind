9cb0d50116943265d0fb593076954e61d76c0035
ImanSaadat <e.saadat72@gmail.com>
2022-06-08 15:38:13 +0430

    about section updated

--- src/components/About/Data.jsx
+++ src/components/About/Data.jsx
@@ -11,18 +11,18 @@ const Data = [
   },
   {
     icon: SiStrapi,
-    title: 'Reliable, tamper-proof network',
-    desc: 'Use decentralization, trusted nodes, premium data, and cryptographic proofs to connect highly accurate and available data/APIs to any smart contract.'
+    title: 'Seamless connection to any API',
+    desc: 'Build on a flexible framework that can retrieve data from any API, connect with existing systems, and integrate with any current or future blockchain.'
   },
   {
     icon: SiFsecure ,
-    title: 'Reliable, tamper-proof network',
-    desc: 'Use decentralization, trusted nodes, premium data, and cryptographic proofs to connect highly accurate and available data/APIs to any smart contract.'
+    title: 'Proven, ready-made solutions',
+    desc: 'Integrate pre-built, time-tested oracle solutions that already secure tens of billions in smart contract value for market-leading decentralized applications.'
   },
   {
     icon: VscServerProcess ,
-    title: 'Reliable, tamper-proof network',
-    desc: 'Use decentralization, trusted nodes, premium data, and cryptographic proofs to connect highly accurate and available data/APIs to any smart contract.'
+    title: 'Secure off-chain computation',
+    desc: 'Use a decentralized network of DeFi Keeper nodes to automate contracts, mitigating risk of manual interventions and centralized servers.'
   },
 
 
