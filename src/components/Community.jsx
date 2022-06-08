02ebb6e870dcf679907e9cdccc0dd1af968c8579
ImanSaadat <e.saadat72@gmail.com>
2022-06-08 16:28:56 +0430

    community section updated

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 const Community = () => {
   return (
-    <section name='community' className='font-Kanit py-20'>
+    <section name='community' className='font-Kanit py-20 bg-community bg-cover bg-center bg-no-repeat bg-blend-multiply bg-black/75'>
       <div className='max-w-[1200px] mx-auto px-5'>
         <div className='flex flex-col justify-center items-center'>
           <h2 className='font-bold text-center text-2xl md:text-5xl'>Join Our DeFi Community</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       },
       boxShadow: {
         'developer': '0 0 12px #00d8ff',
+      },
+      backgroundImage: {
+        'community': "url('/src/assets/subscribe.jpg')",
       }
     },
   },
